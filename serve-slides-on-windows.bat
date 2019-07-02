05aae8bdab9212e6cae5283c5ef30b3ec0a8bdb7
codedifferently <leon@codedifferently>
2019-07-02 15:32:47 -0400

    update

--- serve-slides-on-windows.bat
+++ serve-slides-on-windows.bat
@@ -1,22 +1,31 @@
-#0.
-for /f "tokens=5" %%a in ('netstat -aon ^| find ":8080" ^| find "LISTENING"') do taskkill /f /pid %%a
+# 1. kill process on 8080
+@ECHO OFF                                                                              
+FOR /F "tokens=5" %%T IN ('netstat -a -n -o ^| findstr "9797" ') DO (
+SET /A ProcessId=%%T) &GOTO SkipLine                                                   
+:SkipLine                                                                              
+echo ProcessId to kill = %ProcessId%
+taskkill /f /pid %ProcessId%
+PAUSE
+
+
+#2. delete temporary-directory
 rm -rf temporary-directory
 
-#1. make temporary directory
+#3. create new temporary-directory
 mkdir temporary-directory
 
-#2. change to temporary directory
+#4. change to temporary-directory
 cd temporary-directory
 
-#3. git clone into temp dir
+#5. git clone into temp dir
 git clone https://github.com/CodeDifferently/lecture-slides.git temporary-directory/current-lecture-slides
 
-#4. cd into cloned repo
+#6. cd into cloned repo
 cd current-lecture-slides
 
 #7. open chrome
 start chrome http://localhost:8080/temporary-directory/current-lecture-slides/
 
-#5. serve index.html
+#8. serve index.html
 python -m SimpleHTTPServer 8080
 python -m http.server 8080
