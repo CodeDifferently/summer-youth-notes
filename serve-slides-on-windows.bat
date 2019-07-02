ba2839fd12d7e83bb7c0e0e9004f801a2c2feff2
Route 9 Staff <r9staff@delibrary.priv>
2019-07-02 14:59:10 -0400

    update

---
+++ serve-slides-on-windows.bat
@@ -0,0 +1,22 @@
+#0.
+for /f "tokens=5" %%a in ('netstat -aon ^| find ":8080" ^| find "LISTENING"') do taskkill /f /pid %%a
+rm -rf temporary-directory
+
+#1. make temporary directory
+mkdir temporary-directory
+
+#2. change to temporary directory
+cd temporary-directory
+
+#3. git clone into temp dir
+git clone https://github.com/CodeDifferently/lecture-slides.git temporary-directory/current-lecture-slides
+
+#4. cd into cloned repo
+cd current-lecture-slides
+
+#7. open chrome
+start chrome http://localhost:8080/
+
+#5. serve index.html
+python -m SimpleHTTPServer 8080
+python -m http.server 8080
